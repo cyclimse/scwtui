8044a46ae1e1b5b1cf2643ec1c9a1e376a0a1c07
cyclimse <andy@mery.cloud>
2023-12-03 14:45:44 +0100

    fix(ui): refresh view if search is dirty

--- internal/resource/searcher.go
+++ internal/resource/searcher.go
@@ -2,9 +2,11 @@ package resource
 
 import "context"
 
+type SetOfIDs = map[string]struct{}
+
 type Searcher interface {
 	// Search searches for resources. Returns a list of resource IDs that match the query.
-	Search(ctx context.Context, query string) ([]string, error)
+	Search(ctx context.Context, query string) (SetOfIDs, error)
 
 	// Index indexes a resource.
 	Index(r Resource) error
